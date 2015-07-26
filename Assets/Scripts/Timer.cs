e0c36bf35d2982d66f3e763ee8c0b2b3e261a84f
elmnop <ellana.ria@gmail.com>
2015-07-27 00:20:16 +0800

    timer, 1 and 2 of fire residential scene, added skin for white and blue buttons

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+
+    public Image timerBar;
+    public float increaseFillAmount;
+    //float timekuno = 60;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerBar.fillAmount -= increaseFillAmount * Time.deltaTime;
+        //Debug.Log(timekuno -= Time.deltaTime);
+
+        if (timerBar.fillAmount <= 0)
+        {
+            //Application.LoadLevel("next_scene");
+            //Debug.Log("do nothing");
+        }
+        else
+        {
+            //Debug.Log("do nothing"); 
+        }
+    }
+}
