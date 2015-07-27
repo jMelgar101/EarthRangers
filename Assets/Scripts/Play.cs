04527d4c99bd531ababafd22e9560a5431bdc4f3
unknown <matzuo>
2015-07-28 04:06:32 +0800

    gwaproChampion

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -4,31 +4,14 @@ using System.Collections;
 
 public class Play : MonoBehaviour
 {
-    //public Camera mainCamera;
-    //public Image landingPageRotate = null;
-    //public Texture landingPageBg = null;
+    public Button play;
 
-    public GUISkin skin;
-
-    // Use this for initialization
     void Start()
     {
-
+        play.onClick.AddListener(nextPage);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnGUI()
-    {
-        GUI.skin = skin;
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 50), "PLAY!"))
-        {
-            Application.LoadLevel("createjoin");
-            Debug.Log("Entring create/join team");
-        }
+    void nextPage() {
+        Application.LoadLevel("createjointeam");
     }
 }
